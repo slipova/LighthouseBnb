cbf4b19016f53f1fbf80b93aa634fdf966185988
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-26 02:09:06 +0000

    fix display in my listings

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -138,7 +138,7 @@ const getAllProperties = function(options, limit = 10) {
   }
 
   if (options.owner_id) {
-    queryParams.push(`%${options.owner_id}%`);
+    queryParams.push(`${options.owner_id}`);
     queryString += `AND owner_id = $${queryParams.length} `;
   }
 
